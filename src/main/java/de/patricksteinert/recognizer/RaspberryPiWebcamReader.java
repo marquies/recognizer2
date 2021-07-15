9d9bb6f5ae42187c55dc0f0495c64b527c508a76
Patrick Steinert <me@patrickbreucking.de>
2021-07-15 09:03:21 +0200

    Development

--- src/main/java/de/patricksteinert/recognizer/RaspberryPiWebcamReader.java
+++ src/main/java/de/patricksteinert/recognizer/RaspberryPiWebcamReader.java
@@ -1,28 +1,28 @@
 package de.patricksteinert.recognizer;
 
-import org.bytedeco.javacv.*;
+import org.bytedeco.javacv.Frame;
+import org.bytedeco.javacv.FrameGrabber;
+import org.bytedeco.javacv.OpenCVFrameConverter;
+import org.bytedeco.javacv.OpenCVFrameGrabber;
 import org.bytedeco.opencv.opencv_core.IplImage;
 import org.bytedeco.opencv.opencv_core.Mat;
-import org.bytedeco.opencv.opencv_videoio.VideoCapture;
-import org.bytedeco.javacpp.BytePointer;
 import org.bytedeco.opencv.opencv_core.Size;
+import org.bytedeco.opencv.opencv_videoio.VideoCapture;
 
 import java.io.File;
 
 import static org.bytedeco.opencv.global.opencv_core.cvFlip;
-import static org.bytedeco.opencv.global.opencv_imgcodecs.imwrite;
-import static org.bytedeco.opencv.helper.opencv_imgcodecs.cvSaveImage;
 import static org.bytedeco.opencv.global.opencv_videoio.CAP_GSTREAMER;
+import static org.bytedeco.opencv.helper.opencv_imgcodecs.cvSaveImage;
 import static org.opencv.core.CvType.CV_8UC3;
 
-/**                                                            
+/**
  * Created by Patrick Steinert on 30.06.21.
  */
 public class RaspberryPiWebcamReader implements WebcamReader {
-    final int INTERVAL = 100;///you may use interval
-//    CanvasFrame canvas = new CanvasFrame("Web Cam");
-
     private static final String GSTREAMER_PIPELINE = "nvarguscamerasrc ! \\'video/x-raw(memory:NVMM), width=3280, height=2464, format=(string)NV12, framerate=21/1\\' ! nvvidconv flip-method=0 ! \\'video/x-raw, width=960, height=616, format=(string)BGRx\\' ! videoconvert ! \\'video/x-raw, format=(string)BGR\\' ! appsink wait-on-eos=false max-buffers=1 drop=True";
+    //    CanvasFrame canvas = new CanvasFrame("Web Cam");
+    final int INTERVAL = 100;///you may use interval
 
     public RaspberryPiWebcamReader() {
 
@@ -33,23 +33,26 @@ public class RaspberryPiWebcamReader implements WebcamReader {
     public IplImage readImage() {
         new File("images").mkdir();
         //BytePointer ptr = new BytePointer(GSTREAMER_PIPELINE);
-	System.out.println(GSTREAMER_PIPELINE);
+        System.out.println(GSTREAMER_PIPELINE);
         VideoCapture vc = new VideoCapture(GSTREAMER_PIPELINE, CAP_GSTREAMER);
         System.out.println("Exception Mode: " + vc.getExceptionMode());
         vc.setExceptionMode(true);
         System.out.println("Exception Mode: " + vc.getExceptionMode());
-        System.out.println("Backend Name: " + vc.getBackendName());
-        if(!vc.isOpened()) {
+
+        if (!vc.isOpened()) {
             System.out.println("VideoCapture not opened!");
         }
+        boolean retVal = vc.open(GSTREAMER_PIPELINE, CAP_GSTREAMER);
+        System.out.println("Open Return Value: " + retVal);
+        System.out.println("Backend Name: " + vc.getBackendName());
+
         Mat vcimg = new Mat(new Size(960, 616), CV_8UC3);
         boolean ok = vc.read(vcimg);
         if (ok) {
             System.out.println("Read OK");
-        } 
+        }
 
         //imwrite("images" + File.separator + (0) + "-ab.jpg", vcimg);
-
 
 
         FrameGrabber grabber = new OpenCVFrameGrabber(GSTREAMER_PIPELINE); // 1 for next camera
